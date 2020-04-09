00f1c3b642fa291f789fa57a1dcf42404ae430ee
Deetss <dylan817@live.com>
2020-04-08 21:24:13 -0500

    work on gallery and nav

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require "uikit"
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require rails-ujs
-//= require jquery
 //= require jquery-ui
 //= require activestorage
 //= require turbolinks
