39abf5cf455dde3842cfbf74c32a5112b32761b3
Deetss <dylan817@live.com>
2021-06-01 21:12:30 -0500

    pretty gallery urls

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,3 +1,14 @@
 class Gallery < ApplicationRecord
     has_many_attached :images
+
+    validates_format_of :name, :without => /^\d/
+
+    def to_param
+        title.parameterize
+    end
+
+    
+    def self.find(input)
+        input.to_i == 0 ? find_by_name(input) : super
+    end
 end
