6ac10d61ea1853e11fef640a0d9e639f9392259c
Deetss <dylan817@live.com>
2021-06-05 15:30:42 -0500

    fix gallery show

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -6,7 +6,7 @@ class GalleriesController < ApplicationController
     end
 
     def show
-        @gallery = Gallery.find(params[:id])
+        @gallery = Gallery.find_by_slug(params[:id])
     end
 
     private
