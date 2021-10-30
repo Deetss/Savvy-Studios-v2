8278e650212b20258c36a9195d80618d91be06d0
Deetss <dylan817@live.com>
2021-10-29 19:45:11 -0500

    upgrade to rails 6.1.4, add lazy loading to galleries

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_02_030148) do
+ActiveRecord::Schema.define(version: 2021_10_30_003250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,14 @@ ActiveRecord::Schema.define(version: 2021_06_02_030148) do
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
     t.datetime "created_at", null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "admin_users", force: :cascade do |t|
@@ -75,154 +82,6 @@ ActiveRecord::Schema.define(version: 2021_06_02_030148) do
     t.string "type"
   end
 
-  create_table "fae_changes", id: :serial, force: :cascade do |t|
-    t.integer "changeable_id"
-    t.string "changeable_type"
-    t.integer "user_id"
-    t.string "change_type"
-    t.text "updated_attributes"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["changeable_id"], name: "index_fae_changes_on_changeable_id"
-    t.index ["changeable_type"], name: "index_fae_changes_on_changeable_type"
-    t.index ["user_id"], name: "index_fae_changes_on_user_id"
-  end
-
-  create_table "fae_files", id: :serial, force: :cascade do |t|
-    t.string "name"
-    t.string "asset"
-    t.string "fileable_type"
-    t.integer "fileable_id"
-    t.integer "file_size"
-    t.integer "position", default: 0
-    t.string "attached_as"
-    t.boolean "on_stage", default: true
-    t.boolean "on_prod", default: false
-    t.boolean "required", default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["attached_as"], name: "index_fae_files_on_attached_as"
-    t.index ["fileable_type", "fileable_id"], name: "index_fae_files_on_fileable_type_and_fileable_id"
-  end
-
-  create_table "fae_images", id: :serial, force: :cascade do |t|
-    t.string "name"
-    t.string "asset"
-    t.string "imageable_type"
-    t.integer "imageable_id"
-    t.string "alt"
-    t.string "caption"
-    t.integer "position", default: 0
-    t.string "attached_as"
-    t.boolean "on_stage", default: true
-    t.boolean "on_prod", default: false
-    t.integer "file_size"
-    t.boolean "required", default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["attached_as"], name: "index_fae_images_on_attached_as"
-    t.index ["imageable_type", "imageable_id"], name: "index_fae_images_on_imageable_type_and_imageable_id"
-  end
-
-  create_table "fae_options", id: :serial, force: :cascade do |t|
-    t.string "title"
-    t.string "time_zone"
-    t.string "colorway"
-    t.string "stage_url"
-    t.string "live_url"
-    t.integer "singleton_guard"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["singleton_guard"], name: "index_fae_options_on_singleton_guard", unique: true
-  end
-
-  create_table "fae_roles", id: :serial, force: :cascade do |t|
-    t.string "name"
-    t.integer "position"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "fae_static_pages", id: :serial, force: :cascade do |t|
-    t.string "title"
-    t.integer "position", default: 0
-    t.boolean "on_stage", default: true
-    t.boolean "on_prod", default: false
-    t.string "slug"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["slug"], name: "index_fae_static_pages_on_slug"
-  end
-
-  create_table "fae_text_areas", id: :serial, force: :cascade do |t|
-    t.string "label"
-    t.text "content"
-    t.integer "position", default: 0
-    t.boolean "on_stage", default: true
-    t.boolean "on_prod", default: false
-    t.integer "contentable_id"
-    t.string "contentable_type"
-    t.string "attached_as"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["attached_as"], name: "index_fae_text_areas_on_attached_as"
-    t.index ["contentable_id"], name: "index_fae_text_areas_on_contentable_id"
-    t.index ["contentable_type"], name: "index_fae_text_areas_on_contentable_type"
-    t.index ["on_prod"], name: "index_fae_text_areas_on_on_prod"
-    t.index ["on_stage"], name: "index_fae_text_areas_on_on_stage"
-    t.index ["position"], name: "index_fae_text_areas_on_position"
-  end
-
-  create_table "fae_text_fields", id: :serial, force: :cascade do |t|
-    t.string "contentable_type"
-    t.integer "contentable_id"
-    t.string "attached_as"
-    t.string "label"
-    t.string "content"
-    t.integer "position", default: 0
-    t.boolean "on_stage", default: true
-    t.boolean "on_prod", default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["attached_as"], name: "index_fae_text_fields_on_attached_as"
-    t.index ["contentable_type", "contentable_id"], name: "index_fae_text_fields_on_contentable_type_and_contentable_id"
-    t.index ["on_prod"], name: "index_fae_text_fields_on_on_prod"
-    t.index ["on_stage"], name: "index_fae_text_fields_on_on_stage"
-    t.index ["position"], name: "index_fae_text_fields_on_position"
-  end
-
-  create_table "fae_users", id: :serial, force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
-    t.string "first_name"
-    t.string "last_name"
-    t.integer "role_id"
-    t.boolean "active"
-    t.string "language"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["confirmation_token"], name: "index_fae_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_fae_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_fae_users_on_reset_password_token", unique: true
-    t.index ["role_id"], name: "index_fae_users_on_role_id"
-    t.index ["unlock_token"], name: "index_fae_users_on_unlock_token", unique: true
-  end
-
   create_table "galleries", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -231,27 +90,14 @@ ActiveRecord::Schema.define(version: 2021_06_02_030148) do
     t.string "slug"
   end
 
-  create_table "gallery_images", force: :cascade do |t|
-    t.bigint "gallery_id"
-    t.bigint "image_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "position"
-    t.index ["gallery_id"], name: "index_gallery_images_on_gallery_id"
-    t.index ["image_id"], name: "index_gallery_images_on_image_id"
-  end
-
-  create_table "image_categories", force: :cascade do |t|
-    t.string "name"
-    t.integer "position"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "images", force: :cascade do |t|
+    t.string "name"
     t.string "alt"
+    t.bigint "gallery_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
+    t.index ["gallery_id"], name: "index_images_on_gallery_id"
   end
 
   create_table "packages", force: :cascade do |t|
@@ -285,6 +131,6 @@ ActiveRecord::Schema.define(version: 2021_06_02_030148) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "gallery_images", "galleries"
-  add_foreign_key "gallery_images", "images"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "images", "galleries"
 end
